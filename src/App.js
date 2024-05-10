2ce819e551a504532ad574ffa01059f7b9a4bf74
Bakkori <bakkori990@gmail.com>
2024-05-10 17:58:17 +0300

    Upload and a sign FuelDeal logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import main_logo from './assets/logo.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={main_logo} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
